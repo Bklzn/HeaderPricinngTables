9a6129917d46b1fb1510e16067792db6a7682bb7
Bklzn <adrian_bon@wp.pl>
2025-04-16 15:18:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Header + Pricing Table
+
+This is a task from Lexogrine company. Repository contains simple header with login and pricing table.
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="w-screen h-screen bg-[#2F1893]"></div>;
+}
+
+export default App;
